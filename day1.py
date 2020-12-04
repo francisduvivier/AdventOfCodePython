1399ce96592e1528b913c117170a4a58cb17bd56
Francis Duvivier <francisduvivier@gmail.com>
2020-12-04 17:55:34 +0100

    Solve day1 and day2

---
+++ day1.py
@@ -0,0 +1,22 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def printifOk(n1, n2, n3):
+    if (n1 + n2 + n3) == 2020:
+        print('solution: ' + str(n1 * n2 * n3))
+
+
+def part2():
+    input: str = open('input1.txt', 'r').read()
+    numbers = list(map(int, input.split('\n')))
+    for n1 in numbers:
+        for n2 in numbers:
+            for n3 in numbers:
+                printifOk(n1, n2, n3)
+
+if __name__ == '__main__':
+    print('started\n')
+    part2()
+
+    # See PyCharm help at https://www.jetbrains.com/help/pycharm/
---
+++ day2.py
@@ -0,0 +1,47 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import re
+
+
+def isOk(line: str):
+    groups = re.match(r'(\d+)-(\d+) ([a-z]): ([a-z]+)', line).groups()
+    s = int(groups[0])
+    e = int(groups[1])
+    l = groups[2]
+    p = groups[3]
+    charList = [char for char in p]
+    nbMatches = len(list(filter(lambda letter: letter == l, charList)))
+    return s <= nbMatches <= e
+
+
+def part1():
+    input: str = open('input2.txt', 'r').read()
+    nbOk = len(list(filter(isOk, input.split('\n'))))
+    print('nbOk: ' + str(nbOk))
+
+
+def isOk2(line: str):
+    groups = re.match(r'(\d+)-(\d+) ([a-z]): ([a-z]+)', line).groups()
+    s = int(groups[0])
+    e = int(groups[1])
+    l = groups[2]
+    p = groups[3]
+    charList = [char for char in p]
+
+    return (charList[s - 1] == l) + (charList[e - 1] == l) == 1
+
+
+def part2():
+    input: str = open('input2.txt', 'r').read()
+    nbOk = len(list(filter(isOk2, input.split('\n'))))
+    print('nbOk: ' + str(nbOk))
+
+
+if __name__ == '__main__':
+    print('started\n')
+    part1()
+    part2()
+
+    # See PyCharm help at https://www.jetbrains.com/help/pycharm/
