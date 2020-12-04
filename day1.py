7c582d8f77babbceca72c007158566c5d34cfcf0
Francis Duvivier <francisduvivier@gmail.com>
2020-12-04 18:15:35 +0100

    Small cleanup

--- day1.py
+++ day1.py
@@ -1,7 +1,3 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 def printifOk(n1, n2, n3):
     if (n1 + n2 + n3) == 2020:
         print('solution: ' + str(n1 * n2 * n3))
