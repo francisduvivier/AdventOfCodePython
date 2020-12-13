0becd7790ef01c1754cf62aa3211cb8f7fec8483
Francis Duvivier <francisduvivier@gmail.com>
2020-12-13 06:29:14 +0100

    Solve 2020 day 13 part 1

--- day13.py
+++ day13.py
@@ -3,8 +3,22 @@ import math
 tInput = open('day13-testinput.txt').read().strip().splitlines()
 rInput = open('day13-input.txt').read().strip().splitlines()
 
+
 def part1(input):
-    return
+    time = int(input[0])
+    print(time)
+    busNumber = input[1].split(',')
+    waitTimes = []
+    waitTimesMap = dict()
+    for busNumber in busNumber:
+        if busNumber == 'x': continue
+        busNumber = int(busNumber)
+        waitTime = busNumber - (time % busNumber)
+        waitTimes.append(waitTime)
+        waitTimesMap[waitTime] = busNumber
+        print(busNumber, waitTime)
+    minWaitTime = min(waitTimes)
+    return minWaitTime * waitTimesMap[minWaitTime]
 
 
 if __name__ == '__main__':
