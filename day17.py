2d06b9f31e44b344567a21fe9ae5be03fa111468
Francis Duvivier <francisduvivier@gmail.com>
2020-12-17 09:10:18 +0100

    Add Day 17 Part2

--- day17.py
+++ day17.py
@@ -2,13 +2,19 @@ tInput = open('day17-testinput.txt').read().strip().splitlines()
 rInput = open('day17-input.txt').read().strip().splitlines()
 
 
-def neighbors(active):
+def findNeighbors(active):
     result = []
+    z2Options = [0, -1, 1] if 'z2' in active else [0]
     for row in [-1, 0, 1]:
         for col in [-1, 0, 1]:
             for z in [-1, 0, 1]:
-                if any([row, col, z]):
-                    result.append({'row': active['row'] + row, 'col': active['col'] + col, 'z': active['z'] + z})
+                for z2 in z2Options:
+                    if any([row, col, z, z2]):
+                        nb = {'row': active['row'] + row, 'col': active['col'] + col, 'z': active['z'] + z}
+                        if 'z2' in active:
+                            nb['z2'] = active['z2'] + z2
+                        result.append(nb)
+    assert len(result) == (80 if 'z2' in active else 26)
     return result
 
 
@@ -18,7 +24,8 @@ toKey = str
 def doCycle(actives):
     countMap = dict()
     for active in actives:
-        for nb in neighbors(active):
+        neighbors = findNeighbors(active)
+        for nb in neighbors:
             key = toKey(nb)
             if key in countMap:
                 countMap[key]['count'] += 1
@@ -57,12 +64,23 @@ def part1(input):
     return len(actives)
 
 
+def part2(input):
+    actives: [{'row': int, 'col': int, 'z': int, 'nb': (int or None), 'z2': int}] = []
+    for row, line in enumerate(input):
+        for col, letter in enumerate(line):
+            if letter == '#':
+                actives.append({'row': row, 'col': col, 'z': 0, 'z2': 0})
+    playGame(actives, 6)
+    return len(actives)
+
+
 if __name__ == '__main__':
     part1_t = part1(tInput)
     print(['part1 test', (part1_t)])
     assert part1_t == 112
     part1_r = part1(rInput)
     print(['part1 real', part1_r])
-    # assert part2(tInput2)
-    # part2_r = part2(rInput)
-    # # print(['part2 real', part2_r])
+    assert part1_r == 265
+    assert part2(tInput) == 848
+    part2_r = part2(rInput)
+    print(['part2 real', part2_r])
