5bd3231117df09a6dd31c5f1588adf75f353555a
Francis Duvivier <francisduvivier@gmail.com>
2020-12-18 06:59:33 +0100

    Solve 2020 Day 18 Part 1

--- day18.py
+++ day18.py
@@ -2,18 +2,50 @@ tInput = open('day18-testinput.txt').read().strip().splitlines()
 rInput = open('day18-input.txt').read().strip().splitlines()
 
 
-def calcLine(line):
-    parts = line.split(' ')
-    currString = ''
-    suffix = ''
-    for part in parts:
-        currString += part
-        if part == '*':
-            currString += '('
-            suffix += ')'
-    print(currString, suffix, eval(currString + suffix))
+def calcMapRec(map):
+    return 1
 
-    return eval(currString + suffix)
+
+import re
+
+
+def doCalc(subCalc):
+    print('doCalc subCalc', subCalc)
+    result = subCalc[0]
+    for i in range(1, len(subCalc) - 1, 2):
+        toEval = str(result) + str(subCalc[i]) + str(subCalc[i + 1])
+        print('toEval', toEval)
+        result = eval(toEval)
+    return result
+
+
+def calcLine(line):
+    parts = list(filter(lambda ch: ch != ' ', [char for char in line]))
+    print('parts', parts)
+    deepCalcList = []
+    calcStack = []
+    for part in parts:
+        if part == '(':
+            subCalc = []
+            deepCalcList.append(subCalc)
+            calcStack.append(deepCalcList)
+            deepCalcList = subCalc
+            pass
+        elif part == ')':
+            currCalc = deepCalcList
+            deepCalcList = calcStack.pop()
+            subCalc = deepCalcList.pop()
+            assert subCalc == currCalc
+            subCalcResult = doCalc(subCalc)
+            deepCalcList.append(subCalcResult)
+            pass
+        else:
+            deepCalcList.append(part)
+    print(deepCalcList)
+    result = doCalc(deepCalcList)
+    print([line, deepCalcList, result])
+
+    return result
 
 
 def part1(input):
@@ -24,9 +56,9 @@ def part1(input):
 
 
 if __name__ == '__main__':
-    assert calcLine(tInput[0]) == 26
-    assert calcLine(tInput[1]) == 437
-    assert calcLine(tInput[2]) == 12240
+    # assert calcLine(tInput[0]) == 26
+    # assert calcLine(tInput[1]) == 437
+    # assert calcLine(tInput[2]) == 12240
     assert calcLine(tInput[3]) == 13632
     part1_r = part1(rInput)
     print(['part1 real', part1_r])
