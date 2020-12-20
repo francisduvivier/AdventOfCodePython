5f5aa0d8ea5daf93a07077115c15d9676511839a
Francis Duvivier <francisduvivier@gmail.com>
2020-12-20 06:16:06 +0100

    Start 2020 Day 20

---
+++ day20.py
@@ -0,0 +1,20 @@
+tInput = open('day20-testinput.txt').read().strip().split('Tile ')
+rInput = open('day20-input.txt').read().strip().split('Tile ')
+
+
+def part1(input):
+    pass
+
+
+def part2(input):
+    pass
+
+
+if __name__ == '__main__':
+    assert part1(tInput) == 20899048083289
+    part1_r = part1(rInput)
+    print(['part1 real', part1_r])
+    # assert part1_r == 151
+    # assert part2(tInput) == 2
+    # part2_r = part2(rInput)
+    # print(['part2 real', part2_r])
