236e677b2c62d2dc9ea7a8ac5a5fcc60c3efc9e6
Francis Duvivier <francisduvivier@gmail.com>
2020-12-21 06:16:41 +0100

    Start 2020 Day 21

---
+++ day21.py
@@ -0,0 +1,14 @@
+tInput = open('day21-testinput.txt').read().strip().splitlines()
+rInput = open('day21-input.txt').read().strip().splitlines()
+
+def part1(input):
+    return 0
+
+if __name__ == '__main__':
+    assert part1(tInput) == 'TODO'
+    part1_r = part1(rInput)
+    print(['part1 real', part1_r])
+    # assert part1_r == TODO
+    # assert part2(tInput) == TODO
+    # part2_r = part2(rInput)
+    # print(['part2 real', part2_r])
