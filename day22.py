6a6b7ae80c7a7852407e0580b8f16cf9130b90a8
Francis Duvivier <francisduvivier@gmail.com>
2020-12-22 07:02:07 +0100

    Prepare 2020 Day 22 Part 2

--- day22.py
+++ day22.py
@@ -1,19 +1,28 @@
+from util import mapl
+
 tInput = open('day22-testinput.txt').read().strip()
 rInput = open('day22-input.txt').read().strip()
-from util import mapl
 import numpy as np
+
+
+def parseCards(input):
+    [_, p1CardStr, p2CardStr] = input.split(':')
+    p1Cards: list[int] = mapl(int, p1CardStr.split('\n\n')[0].strip().splitlines())
+    p2Cards: list[int] = mapl(int, p2CardStr.strip().splitlines())
+    p1Cards.reverse()
+    p2Cards.reverse()
+    return p1Cards, p2Cards
 
 
 def part1(input):
     p1Cards, p2Cards = parseCards(input)
-
-    mostCards = playTillFinished(p1Cards, p2Cards)
+    mostCards = playTillFinishedPart1(p1Cards, p2Cards)
     result = sum([val * (i + 1) for (i, val) in enumerate(mostCards)])
     print('p1res', result)
     return result
 
 
-def playTillFinished(p1Cards, p2Cards):
+def playTillFinishedPart1(p1Cards, p2Cards):
     while len(p1Cards) and len(p2Cards):
         p1Card = p1Cards.pop()
         p2Card = p2Cards.pop()
@@ -30,13 +39,19 @@ def playTillFinished(p1Cards, p2Cards):
     return mostCards
 
 
-def parseCards(input):
-    [_, p1CardStr, p2CardStr] = input.split(':')
-    p1Cards: list[int] = mapl(int, p1CardStr.split('\n\n')[0].strip().splitlines())
-    p2Cards: list[int] = mapl(int, p2CardStr.strip().splitlines())
-    p1Cards.reverse()
-    p2Cards.reverse()
-    return p1Cards, p2Cards
+def playTillFinishedPart2(p1Cards, p2Cards):
+    while len(p1Cards) and len(p2Cards):
+        break
+    mostCards = p1Cards if len(p1Cards) > len(p2Cards) else p2Cards
+    return mostCards
+
+
+def part2(input):
+    p1Cards, p2Cards = parseCards(input)
+    mostCards = playTillFinishedPart2(p1Cards, p2Cards)
+    result = sum([val * (i + 1) for (i, val) in enumerate(mostCards)])
+    print('p1res', result)
+    return result
 
 
 # print('parseCards(tInput)', parseCards(tInput))
@@ -46,7 +61,7 @@ if __name__ == '__main__':
     assert part1(tInput) == 306
     part1_r = part1(rInput)
     print(['part1 real', part1_r])
-    # assert part1_r == 2282
-    # assert part2(tInput) == 'mxmxvkd,sqjhc,fvjkl'
-    # part2_r = part2(rInput)
-    # print(['part2 real', part2_r])
+    assert part1_r == 31308
+    assert part2(tInput) == 291
+    part2_r = part2(rInput)
+    print(['part2 real', part2_r])
