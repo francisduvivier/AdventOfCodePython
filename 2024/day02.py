3a349c91e723f00f45e2f87257ac19a7aab560e5
Francis Duvivier <francis.duvivier@persgroep.net>
2024-12-02 12:30:52 +0100

    Start day02

---
+++ 2024/day02.py
@@ -0,0 +1,20 @@
+import time
+
+tInput = open('dayxx-testinput.txt').read().strip()
+rInput = open('day02-input.txt').read().strip()
+def mapToNumbers(arr):
+    return list(map(int, arr))
+
+def parseInput(input):
+    lines = input.split('\n')
+    lineToNumbers = lambda line: mapToNumbers(line.split(' '))
+    numbersMatrix = list(map(lineToNumbers, lines))
+    return numbersMatrix
+
+
+def part1(input):
+    parsed = parseInput(input)
+    print(parsed)
+
+part1(tInput)
+part1(rInput)
