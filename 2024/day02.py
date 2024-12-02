ae84bf2b6e781dc181f2f8d6d813fea87ddec57b
Francis Duvivier <francis.duvivier@persgroep.net>
2024-12-02 12:38:26 +0100

    Add day02 part 1

--- 2024/day02.py
+++ 2024/day02.py
@@ -2,19 +2,46 @@ import time
 
 tInput = open('dayxx-testinput.txt').read().strip()
 rInput = open('day02-input.txt').read().strip()
+
+
 def mapToNumbers(arr):
     return list(map(int, arr))
 
+
+lineToNumbers = lambda line: mapToNumbers(line.split(' '))
+
+
 def parseInput(input):
-    lines = input.split('\n')
-    lineToNumbers = lambda line: mapToNumbers(line.split(' '))
-    numbersMatrix = list(map(lineToNumbers, lines))
-    return numbersMatrix
+    reportLines = input.split('\n')
+    reportMatrix = list(map(lineToNumbers, reportLines))
+    return reportMatrix
+
+
+def isSafe(reportLine):
+    prevVal = None
+    direction = reportLine[0] > reportLine[1]
+    for val in reportLine:
+        if prevVal == val:
+            return False
+        if prevVal is not None:
+            newDirection = prevVal > val
+            if newDirection != direction:
+                return False
+            if abs(prevVal - val) > 3:
+                return False
+        prevVal = val
+    return True
 
 
 def part1(input):
     parsed = parseInput(input)
-    print(parsed)
+    safeReports = 0
+    for reportLine in parsed:
+        if isSafe(reportLine):
+            safeReports += 1
+
+    print(safeReports)
+
 
 part1(tInput)
 part1(rInput)
