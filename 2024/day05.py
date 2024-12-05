725a14ade493a30fecbf1dda2b84b72fc09d6cf7
Francis Duvivier <francis.duvivier@persgroep.net>
2024-12-05 13:12:21 +0100

    Fix day 5 part 2

--- 2024/day05.py
+++ 2024/day05.py
@@ -16,6 +16,7 @@ def parseInput(input):
     sequences = mapl(lambda line: line.split(','), sequences.splitlines())
     return (rules, sequences)
 
+
 def create_before_map(rules):
     key_is_before_set_map = {}
     for rule in rules:
@@ -25,6 +26,7 @@ def create_before_map(rules):
         key_is_before_set_map[before].add(after)
     return key_is_before_set_map
 
+
 def follows_rules(sequence, rules):
     # print(sequence)
     done_numbers_set = set()
@@ -32,12 +34,11 @@ def follows_rules(sequence, rules):
     for el in sequence:
         must_be_after_set = key_is_before_set_map[el] if el in key_is_before_set_map else set()
         broken_rules = must_be_after_set.intersection(done_numbers_set)
-        if len(broken_rules)>0:
+        if len(broken_rules) > 0:
             return False
         done_numbers_set.add(el)
 
     return True
-
 
 
 def part1(input):
@@ -51,8 +52,10 @@ def part1(input):
             result += int(sequence[int((len(sequence) - 1) / 2)])
     print(result)
 
+
 part1(tInput)
 part1(rInput)
+
 
 def fix(sequence, rules):
     fixed_sequence = []
@@ -62,17 +65,19 @@ def fix(sequence, rules):
         must_be_after_set = key_is_before_set_map[el] if el in key_is_before_set_map else set()
         broken_rules = must_be_after_set.intersection(done_numbers_set)
 
-        if len(broken_rules)>0:
+        if len(broken_rules) > 0:
             first_broken_rule_index = 0
             for i in range(len(fixed_sequence)):
                 if fixed_sequence[i] in broken_rules:
                     first_broken_rule_index = i
-            fixed_sequence = fixed_sequence[:first_broken_rule_index -1] + [el] + fixed_sequence[first_broken_rule_index:]
+            fixed_sequence = fixed_sequence[:first_broken_rule_index - 1] + [el] + fixed_sequence[
+                                                                                   first_broken_rule_index - 1:]
         else:
             fixed_sequence.append(el)
         done_numbers_set.add(el)
 
     return fixed_sequence
+
 
 def part2(input):
     (rules, sequences) = parseInput(input)
@@ -85,6 +90,7 @@ def part2(input):
                 sequence = fix(sequence, rules)
             result += int(sequence[int((len(sequence) - 1) / 2)])
     print(result)
+
 
 part2(tInput)
 part2(rInput)
