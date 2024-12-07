f5214b5e586fed510b61811b0f51a95defc36470
Francis Duvivier <francis.duvivier@persgroep.net>
2024-12-07 07:31:27 +0100

    Solve Day07 part2

--- 2024/day07.py
+++ 2024/day07.py
@@ -5,6 +5,7 @@ from util import map_to_numbers
 tInput = open('day07-testinput.txt').read().strip()
 rInput = open('day07-input.txt').read().strip()
 
+
 def parseInput(input):
     lines = input.splitlines()
     equations = [line.split(': ') for line in lines]
@@ -13,27 +14,19 @@ def parseInput(input):
     return equations
 
 
-operators = ['+', '*']
-
-
 def invert_operator(left, last, operator):
     if operator == '+':
         return left - last
     if operator == '*':
         return left / last
+    if operator == '||':
+        cut_off = str(int(left))[: -len(str(last))]
+        cut_off = '0' if cut_off == '' else cut_off
+        return float(cut_off if str(int(left)).endswith(str(last)) else -1)
     return None
 
 
-def apply_operator(left, right, operator):
-    if operator == '+':
-        return left + right
-    if operator == '*':
-        return left * right
-    pass
-
-
-def fill_operators_rec(left: float, right: list[int]) -> None or list[str]:
-    global operators
+def fill_operators_rec(left: float, right: list[int], operators) -> None or list[str]:
     if left < 0:
         return None
     if not left.is_integer():
@@ -42,8 +35,9 @@ def fill_operators_rec(left: float, right: list[int]) -> None or list[str]:
         return [] if left == right[0] else None
 
     for operator in operators:
+        new_right = right[:-1]
         new_left = invert_operator(left, right[-1], operator)
-        solution = fill_operators_rec(new_left, right[:-1])
+        solution = fill_operators_rec(new_left, new_right, operators)
         if solution is not None:
             return [operator] + solution
     return None
@@ -51,11 +45,26 @@ def fill_operators_rec(left: float, right: list[int]) -> None or list[str]:
 
 def part1(input):
     equations = parseInput(input)
-    result = int(sum([left for (left, right) in equations if fill_operators_rec(left, right) is not None]))
+    operators_part1 = ['+', '*']
+    result = int(
+        sum([left for (left, right) in equations if fill_operators_rec(left, right, operators_part1) is not None]))
+    print(result)
+    return result
+
+
+# assert part1(tInput) == 3749
+# part1(rInput)
+
+
+def part2(input):
+    equations = parseInput(input)
+    operators_part2 = ['+', '*', '||']
+    result = int(
+        sum([left for (left, right) in equations if fill_operators_rec(left, right, operators_part2) is not None]))
 
     print(result)
     return result
 
 
-assert part1(tInput) == 3749
-part1(rInput)
+assert part2(tInput) == 11387
+part2(rInput)
