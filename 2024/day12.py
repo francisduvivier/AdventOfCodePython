b16ef3300d1b88345eca14d40e7fe14d3f720810
Francis Duvivier <francis.duvivier@persgroep.net>
2024-12-12 19:03:22 +0100

    Try solve day 12 part 2

--- 2024/day12.py
+++ 2024/day12.py
@@ -2,8 +2,7 @@ import time
 
 import numpy as np
 
-from grid_robot import yx_key, GridRobot, DIRS
-from util import map_to_numbers
+from grid_robot import yx_key, GridRobot, DIRS, parse_state_key, DIR_LETTERS
 
 tInput0 = open('day12-testinput0.txt').read().strip()
 tInput = open('day12-testinput.txt').read().strip()
@@ -15,24 +14,25 @@ def parseInput(input):
     char_matrix = np.array([[char for char in line] for line in lines])
     return char_matrix
 
+
 def out_of_bounds(char_matrix, robot):
     return robot.y < 0 or robot.y >= len(char_matrix) or robot.x < 0 or robot.x >= len(char_matrix[robot.y])
 
 
 def find_patch_rec(row, col, garden, patch):
-    robot = GridRobot(row, col)
-    if robot.yx_key() in patch["tile_set"]:
+    row_col_key = yx_key(row, col)
+    if row_col_key in patch["tile_set"]:
         return
-    patch["tile_set"].add(robot.yx_key())
+    patch["tile_set"].add(row_col_key)
     for dir in DIRS:
-        new_robot = GridRobot(row, col, dir)
+        new_robot = GridRobot(col, row, dir)
         new_robot.move_forward()
-        if out_of_bounds(garden, new_robot) or garden[new_robot.y][new_robot.x] != patch["letter"]:
+        patch_letter = patch["letter"]
+        if out_of_bounds(garden, new_robot) or garden[new_robot.y][new_robot.x] != patch_letter:
             new_robot.move_backward()
             patch["wall_set"].add(new_robot.state_key())
         else:
             find_patch_rec(new_robot.y, new_robot.x, garden, patch)
-
 
 
 def find_patch(row, col, garden):
@@ -59,6 +59,14 @@ def part1(input):
     # we also keep a list of patches
     # then we can calc for each patch the cost: nbTiles x nbWalls
     garden = parseInput(input)
+    patches = find_patches(garden)
+
+    result = sum([calc_price(patch) for patch in patches])
+    print(result)
+    return result
+
+
+def find_patches(garden):
     done_tile_set = set()
     patches = []
 
@@ -75,12 +83,68 @@ def part1(input):
             print('doing ', row, col, letter)
             patch = find_patch(row, col, garden)
             add_patch(patch)
+    return patches
 
-    result = sum([calc_price(patch) for patch in patches])
+
+# assert part1(tInput0) == 140
+# assert part1(tInput) == 1930
+# part1(rInput)
+
+
+def find_disconnected_walls(wall_set):
+    walls = [parse_state_key(wall_key) for wall_key in wall_set]
+    nb_disconnected = 0
+    for dir in DIR_LETTERS:
+        walls_for_dir = [wall for wall in walls if wall[2] == dir]
+        x_index = 1
+        y_index = 0
+        x_values = set([wall[x_index] for wall in walls_for_dir])
+        for x in x_values:
+            sorted_walls_on_line = sorted([wall for wall in walls_for_dir if wall[x_index] == x],
+                                          key=lambda wall: wall[y_index])
+            curr_y = None
+            for sorted_wall in sorted_walls_on_line:
+                if curr_y != sorted_wall[y_index] - 1:
+                    print('found disconnect for dir', dir, 'x', x, 'curr_y', curr_y, '->', sorted_wall[y_index])
+                    nb_disconnected += 1
+                curr_y = sorted_wall[y_index]
+
+        y_values = set([wall[y_index] for wall in walls_for_dir])
+        for y in y_values:
+            sorted_walls_on_line = sorted([wall for wall in walls_for_dir if wall[y_index] == y],
+                                          key=lambda wall: wall[x_index])
+            curr_x = None
+            for sorted_wall in sorted_walls_on_line:
+                if curr_x != sorted_wall[x_index] - 1:
+                    nb_disconnected += 1
+                    print('found disconnect for dir', dir, 'x', x, 'curr_y', curr_y, '->', sorted_wall[y_index])
+                curr_x = sorted_wall[x_index]
+
+        print(walls_for_dir)
+    return nb_disconnected
+
+
+def calc_price2(patch):
+    print('calc_price2: ' + patch['letter'])
+    return find_disconnected_walls(patch["wall_set"])
+
+
+def part2(input):
+    # for wall in patch
+    # for y in rows
+    # get_disconnected_y(walls,y)
+
+    # for x in cols
+    # get_disconnected_x(wall,x)
+    garden = parseInput(input)
+    patches = find_patches(garden)
+
+    result = sum([calc_price2(patch) for patch in patches])
+
     print(result)
     return result
 
 
-assert part1(tInput0) == 140
-assert part1(tInput) == 1930
-part1(rInput)
+assert part2(tInput0) == 80
+assert part2(tInput) == 1206
+part2(rInput)
--- grid_robot.py
+++ grid_robot.py
@@ -5,6 +5,7 @@ DIR = {
     '<': {'dx': -1, 'dy': 0},
 }
 DIRS = [DIR[key] for key in DIR.keys()]
+DIR_LETTERS = [key for key in DIR.keys()]
 
 
 class GridRobot:
@@ -12,6 +13,7 @@ class GridRobot:
         self.x = x
         self.y = y
         self.dyx = dyx
+        self.dir_index = DIRS.index(self.dyx)
 
     def move_forward(self):
         self.x += self.dyx['dx']
@@ -22,16 +24,23 @@ class GridRobot:
         self.y += -self.dyx['dy']
 
     def turn_right(self):
-        self.dyx = DIRS[(DIRS.index(self.dyx) + 1) % len(DIRS)]
+        self.dyx = DIRS[(self.dir_index + 1) % len(DIRS)]
 
     def __str__(self):
-        return f'dyx{self.dyx},x{self.y},y{self.x}'
+        return self.state_key()
 
     def state_key(self):
-        return f'{self.dyx},{self.yx_key()}'
+        return f'{self.yx_key()}:{DIR_LETTERS[self.dir_index]}'
 
     def yx_key(self):
-        return yx_key(self.y,self.x)
+        return yx_key(self.y, self.x)
 
-def yx_key(y,x):
+
+def yx_key(y, x):
     return f'{y},{x}'
+
+
+def parse_state_key(state_key: str):
+    split1 = state_key.split(':')
+    split2 = split1[0].split(',')
+    return int(split2[0]), int(split2[1]), split1[1],
