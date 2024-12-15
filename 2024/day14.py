c714701fe647aec6d5ff48ae6e7272749b794738
Francis Duvivier <francis.duvivier@persgroep.net>
2024-12-15 09:53:45 +0100

    Solve day 14 part 2

--- 2024/day14.py
+++ 2024/day14.py
@@ -1,11 +1,9 @@
 import re
-import time
 
 import numpy as np
 from dotmap import DotMap
 
 from grid_robot import GridRobot
-from util import map_to_numbers, sub_map_to_numbers
 
 test_input = open('day14-testinput.txt').read().strip()
 real_input = open('day14-input.txt').read().strip()
@@ -38,7 +36,7 @@ def get_quadrant(robot):
 
 
 def part1(input, grid_height, grid_width):
-    grid =    grid = [[x for x in range(grid_width)] for y in range(grid_height)]
+    grid = np.array([['.' for x in range(grid_width)] for y in range(grid_height)])
     robots = parse_input(input, grid)
     for second in range(100):
         for robot in robots:
@@ -63,3 +61,52 @@ def part1(input, grid_height, grid_width):
 assert part1(test_input, 7, 11) == 12
 print('Check success!')
 part1(real_input, 103, 101)
+
+
+def get_grid(robots):
+    grid = robots[0].grid.copy()
+    for robot in robots:
+        grid[robot.y][robot.x] = '#'
+    return grid
+
+
+def part2(input, grid_height, grid_width):
+    grid = np.array([['.' for x in range(grid_width)] for y in range(grid_height)])
+    robots = parse_input(input, grid)
+    state_map = dict()
+    min_score = 2 ** 32
+    min_score_time = 0
+    min_score_grid = None
+    for second in range(1, 10_000_000_000):
+        state = ''
+        for robot in robots:
+            robot.move_forward()
+            state += str(robot)
+
+        score = cluster_score(robots)
+        if not min_score or min_score > score:
+            min_score = score
+            min_score_time = second
+            min_score_grid = get_grid(robots)
+        if state in state_map:
+            result = state_map[state]
+            print('found repeat at' + str(second) + ' for second' + str(result))
+            print('\n'.join([''.join(line) for line in min_score_grid]))
+            print('min_score_time', min_score_time)
+            return min_score_time
+        state_map[state] = second
+    raise Exception('no solution')
+
+
+def cluster_score(robots):
+    rxs = [robot.x for robot in robots]
+    median_x = np.median(rxs)
+    msx = np.sum(np.square([median_x - other for other in rxs]))
+    rys = [robot.y for robot in robots]
+    median_y = np.median(rys)
+    msy = np.sum(np.square([median_y - other for other in rys]))
+    return msx + msy
+
+# part2(test_input, 7, 11)
+print('test_input success')
+part2(real_input, 103, 101)
