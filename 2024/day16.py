073721b4639870a599a5280bbb743f4f2680be36
Francis Duvivier <francis.duvivier@persgroep.net>
2024-12-16 20:59:21 +0100

    Implement day 16 part 2 with bread-first loop instead of recursion

--- 2024/day16.py
+++ 2024/day16.py
@@ -22,52 +22,7 @@ def parse_input(input):
     return char_matrix
 
 
-# def find_best_rec(robot: GridRobot, end, calc_state):
-#     min_turn_cost = 1000 if robot.x != end.x and robot.y != end.y else 0
-#     if calc_state.min < robot.cost + abs(robot.x - end.x) + abs(
-#             robot.y - end.y) + min_turn_cost:
-#         return
-#     end_found = robot.y == end.y and robot.x == end.x
-#     min_cost_here = calc_state.min_map[robot.state_key()] if robot.state_key() in calc_state.min_map else None
-#     if min_cost_here is not None and min_cost_here < robot.cost:
-#         return
-#     if min_cost_here is not None and min_cost_here == robot.cost:
-#         if robot.yx_key() in calc_state.all_tiles:
-#             for tile in robot.path_tiles:
-#                 calc_state.all_tiles.add(tile)
-#         return
-#     if DEBUG: print('find_best_rec', robot, robot.cost)
-#     calc_state.min_map[robot.state_key()] = robot.cost
-#     if end_found:
-#         if robot.cost < calc_state.min:
-#             calc_state.all_tiles = set()
-#         for tile in robot.path_tiles:
-#             calc_state.all_tiles.add(tile)
-#         calc_state.min = robot.cost
-#         return
-#     left_robot = robot.clone()
-#     left_robot.turn_right(-1)
-#     if left_robot.state_key() not in calc_state.min_map  or calc_state.min_map[
-#         left_robot.state_key()] > left_robot.cost + 1:
-#         calc_state.min_map[left_robot.state_key()] = left_robot.cost + 1
-#     clean_robot = robot.clone()
-#     for _turn in [1, 1]:
-#         clean_robot.turn_right()
-#         if clean_robot.state_key() not in calc_state.min_map  or calc_state.min_map[
-#             clean_robot.state_key()] > clean_robot.cost + 1:
-#             calc_state.min_map[clean_robot.state_key()] = clean_robot.cost + 1
-#     for turn in [3, 1]:
-#         turn_robot = robot.clone()
-#         turn_robot.turn_right(turn)
-#         find_best_rec(turn_robot, end, calc_state)
-#     move_robot = robot.clone()
-#     move_robot.move_forward()
-#     if not move_robot.out_of_bounds() and move_robot.tile_value() != '#':
-#         find_best_rec(move_robot, end, calc_state)
-#     return
-
-
-def get_next_states(robot: GridRobot)-> list[GridRobot]:
+def get_next_states(robot: GridRobot) -> list[GridRobot]:
     next_states = []
     for turn in [3, 1]:
         turn_robot = robot.clone()
@@ -83,7 +38,7 @@ def get_next_states(robot: GridRobot)-> list[GridRobot]:
 
 def insert_sorted(next_states, param, key):
     next_states.append(param)
-    next_states.sort(key= key)
+    next_states.sort(key=key)
     pass
 
 
@@ -91,7 +46,7 @@ def part12(input):
     grid = parse_input(input)
     start = find_value('S', grid)
     end = DotMap({"y": find_value('E', grid)[0],
-                      "x": find_value('E', grid)[1]})
+                  "x": find_value('E', grid)[1]})
     calc_state = DotMap({"min": 2 ** 30})
     calc_state.min_map = {}
     calc_state.all_tiles = set()
@@ -99,40 +54,50 @@ def part12(input):
     sorted_states_to_try = [start_robot.state_key()]
     found_states = {}
     found_states[start_robot.state_key()] = start_robot
+    eq_map = {}
+
     def heuristic(robot: GridRobot):
         min_turn_cost = 1000 if robot.x != end.x and robot.y != end.y else 0
-        return abs(robot.x - end.x) + abs(robot.y - end.y) + min_turn_cost
+        return robot.cost + abs(robot.x - end.x) + abs(robot.y - end.y) + min_turn_cost
 
-    min_result = None
+    best_robot = None
+
     def key_heuristic(robot_key: str):
         robot = found_states[robot_key]
         min_turn_cost = 1000 if robot.x != end.x and robot.y != end.y else 0
         return abs(robot.x - end.x) + abs(robot.y - end.y) + min_turn_cost
+
     while len(sorted_states_to_try) > 0:
         next_states = get_next_states(found_states[sorted_states_to_try.pop()])
         for next_r in next_states:
-            if min_result is not None and heuristic(next_r) >= min_result:
-                continue
-            end_found = next_r.y == end.y and next_r.x == end.x
-            if end_found:
-                min_result = next_r.cost
-                print('found path', min_result)
+            if best_robot is not None and heuristic(next_r) > best_robot.cost:
                 continue
             if next_r.state_key() not in found_states:
                 found_states[next_r.state_key()] = next_r
                 insert_sorted(sorted_states_to_try, next_r.state_key(), key=key_heuristic)
+                eq_map[next_r.state_key()] = [next_r]
             elif found_states[next_r.state_key()].cost > next_r.cost:
                 found_states[next_r.state_key()] = next_r
                 if next_r.state_key() in sorted_states_to_try:
                     sorted_states_to_try.remove(next_r.state_key())
                 insert_sorted(sorted_states_to_try, next_r.state_key(), key=key_heuristic)
-
-    result = len(calc_state.all_tiles)
+                eq_map[next_r.state_key()] = [next_r]
+            elif found_states[next_r.state_key()].cost == next_r.cost:
+                eq_map[next_r.state_key()].append(next_r)
+            end_found = next_r.y == end.y and next_r.x == end.x
+            if end_found:
+                best_robot = next_r
+                print('found path', best_robot, best_robot.cost)
+    result_set = set(best_robot.path_tiles)
+    for path_item in best_robot.path:
+        for eq_robot in eq_map[path_item]:
+            result_set = result_set.union(set(eq_robot.path_tiles))
+    result = len(result_set)
     print('result', result)
-    print('min_result', min_result)
-    return result, min_result
+    print('best_robot', best_robot.cost)
+    return result, best_robot.cost
 
 
-assert part12(test_input) == (0, 7036)
-assert part12(test_input2) == (0, 11048)
+assert part12(test_input) == (45, 7036)
+assert part12(test_input2) == (64, 11048)
 part12(real_input)
